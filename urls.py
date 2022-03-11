e7450578a70a23489ca41bc90248ad57a9f7ecfb
malcolmcouncil38 <malcboss38@gmail.com>
2022-03-11 17:24:48 -0500

    changes to urls and base

--- urls.py
+++ urls.py
@@ -2,4 +2,13 @@
 from unicodedata import name
 from django.urls import URLPattern, path 
 from . import views
-URLPatterns = []
+
+urlpatterns = [
+  path('', views, name='welcome'),
+  path('home/', views.home, name='home'),
+  path('GameDev/', views.game, name='game'),
+  path('Robotics/', views.robot, name='robot'),
+  path('WebDev', views.web, name='web'),
+  path('announcements/', views.announce, name='anounce'),
+  path('Attendence/', views.attend, name='attend')
+]
